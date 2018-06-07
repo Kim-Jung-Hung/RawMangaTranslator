47acdff1a85ce85503c393354f4f416a9763d802
Jan Guillermo (RIT Student) <jg7934@ad.rit.edu>
2018-06-07 12:35:15 -0400

    Refactor

--- README.md
+++ README.md
@@ -3,30 +3,24 @@
 ## Inspiration
 One early Wednesday morning, I woke up excitedly to read a new chapter on One Piece. However, there was no translated text yet. With anger, frustration, bargaining, depression, and acceptance, we decided to start this project to prevent this from happening again.
 
-## What it does
-As of now, it asks for an image URL (specifically a link to one of the chapter's image in some manga) and the program will show you what text are on the image, then the text will be translated afterward.
-
-## How we built it
+## How I built it
 - Python 3
-- Microsoft's Cognitive Services (We used their Computer Vision API to extract the text from the image)
-- Google Translate API
+- Microsoft's Cognitive Services (for extracting text)
+- Google Translate API (for translating text)
 
-## Challenges we ran into
-- Coming up with ideas. We finally came up with one after midnight. 
-- Productivity crash
-- Had to use Google Translate API because we couldn't figure out how to get the API key working for Microsoft's Translator API
-- API Quota Limit
+## How to use the application
+### Prerequistites
+- Install [Python 3+](https://www.python.org/downloads/)
+- Get an API key for [Microsoft's Cognitive Services](https://azure.microsoft.com/en-us/services/cognitive-services/)
+- Get an API key for [Google Translate API](https://cloud.google.com/translate/docs/)
 
-## Accomplishments that we're proud of
-- Making this program
-- Teamwork
-- Learning a lot of cool things about APIs!
+### Steps
+1. Clone the repository.
+2. Open the command line and navigate to the repository.
+3. Type `pip install -r requirements.txt` and enter.
+4. Open `config.ini` in a code editor and put your API keys for both APIs.
+5. Run the application using `python main.py --url IMAGE_URL`.
 
-## What we learned
-- Try to come up with an idea before a hackathon
-
-## What's next for Raw Manga Translator
-- Figure out a way to get all of the images from a chapter
-- Improve the translated text
-- Add a GUI
-- Edit the image directly to change the raw text to translated text
+## Future Work
+- Replace the text inside the image with translated text
+- Improve the translations
--- main.py
+++ main.py
@@ -1,37 +1,63 @@
 import requests
-import json
-from pprint import pprint
+import configparser
+import argparse
 
-MICROSOFT_API_KEY = "YOUR API KEY"
-GOOGLE_API_KEY = "YOUR API KEY"
 
-image_url = input("Enter an image URL to one of the pages in a manga chapter: ")
-headers = {"ocp-apim-subscription-key": MICROSOFT_API_KEY, "content-type": "application/json"}
-payload = {"language": "unk", "orientation": "true"}
-data = {"url": image_url}
+class RawMangaTranslator:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("config.ini")
 
-r = requests.post("https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr", params=payload, headers=headers, data=json.dumps(data))
+    def translate(self, image):
+        sentences = []
+        translated_sentences = []
 
-result = r.json()
-sentences = []
-translated_sentences = []
+        # Microsoft API Variables
+        headers = {
+            "Ocp-Apim-Subscription-Key": self.config["MicrosoftAPI"]["KEY"]}
+        payload = {"detectOrientation": "true"}
+        data = {"url": image}
 
-for s in result["regions"][0]["lines"]:
-    sentence = ""
+        # First, we'll need to extract the words from the image using Microsoft OCR API
+        request = requests.post(self.config["MicrosoftAPI"]["URL"],
+                                params=payload, headers=headers, json=data)
+        result = request.json()
 
-    for word in s["words"]:
-        sentence += word["text"]
+        # Add sentences to the array so we can translate it later
+        for s in result["regions"][0]["lines"]:
+            sentence = ""
 
-    sentences.append(sentence)
+            for word in s["words"]:
+                sentence += word["text"]
 
-pprint(sentences)
+            sentences.append(sentence)
 
-for s in sentences:
-    data = {"target": "en", "key": GOOGLE_API_KEY, "q": s}
-    r = requests.get("https://translation.googleapis.com/language/translate/v2", params=data)
-    result = r.json()
-    
-    sentence = r.json()["data"]["translations"][0]["translatedText"]
-    translated_sentences.append(sentence)
+        print(sentences)
 
-pprint(translated_sentences)
+        # Finally, we'll translate the sentences extracted from the image!
+        for sentence in sentences:
+            data = {"target": "en",
+                    "key": self.config["GoogleAPI"]["KEY"],
+                    "q": sentence}
+
+            request = requests.get(
+                self.config["GoogleAPI"]["URL"], params=data)
+            result = request.json()
+
+            translated_sentences.append(
+                result["data"]["translations"][0]["translatedText"])
+
+        print(translated_sentences)
+
+
+# Now, let us start the main show!
+if __name__ == "__main__":
+    # Set arguments for this program
+    parser = argparse.ArgumentParser(description="Translates text in raw manga chapter images.")
+    parser.add_argument("--url", help="enter the url of the image you want translated")
+    args = parser.parse_args()
+
+    if (args.url):
+        translator = RawMangaTranslator()
+
+        translator.translate(args.url)
